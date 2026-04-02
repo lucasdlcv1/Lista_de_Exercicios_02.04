3efe598b87d54096c452b28494cf3dc28e0d1128
Lucas de Lima <lucasdlcvieira@gmail.com>
2026-04-02 20:25:39 -0300

    Ex1 finalizado

--- Exercicio01.ConsoleApp/Program.cs
+++ Exercicio01.ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Digite a unidade de medida:");
+Console.WriteLine("m (metros)\ncm (centimetros) \nmm (milimetros)");
+string UM = Console.ReadLine();
+Console.Write("\nDigite a largura da caixa: ");
+double L = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite o comprimento da caixa: ");
+double C = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite a altura da caixa: ");
+double H = Convert.ToDouble(Console.ReadLine());
+
+double Vol = L * C * H;
+
+Console.WriteLine($"\nO Volume da caixa é {Vol}{UM}³");
