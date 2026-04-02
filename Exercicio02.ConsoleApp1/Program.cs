a6e307d61a5fbc613dab138b11952e8eb8014904
Lucas de Lima <lucasdlcvieira@gmail.com>
2026-04-02 20:45:02 -0300

    Exercicio 2 Feito

---
+++ Exercicio02.ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Digite a quilometragem inicial do veículo: ");
+int kmini = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite a quilometragem final do veículo: ");
+int kmfim = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite a quantidade de combustivel consumida: ");
+int consumo = Convert.ToInt32(Console.ReadLine());
+
+int consumomed = (kmfim - kmini)/ consumo;
+
+Console.WriteLine("O consumo medio do veículo foi de: " + consumomed + " km/L");
