431f60237eea03efd9f8c126863587f27297e7dc
Lucas de Lima <lucasdlcvieira@gmail.com>
2026-04-02 21:10:21 -0300

    Feito exercício 3

---
+++ Exercicio03.ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite a temperatura em graus Celsius: ");
+double tempCels= Convert.ToDouble(Console.ReadLine());
+
+double TempFah = (tempCels * 1.8) + 32;
+
+Console.WriteLine($"A temperatura equivalente em Fahrenheit é " + TempFah);
+Console.ReadLine();
+
+
